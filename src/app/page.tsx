4ce9dcef74a76109ff7d57127e4d9b517a29b186
Lee Pierson <awesomehenge@gmail.com>
2023-08-17 15:24:15 +0100

    5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default async function Home() {
-  const res = await fetch("https://api.github.com/repos/GreedECrow/class10", {next: {revalidate: 15}});
-  const data = await res.json();
-console.log(data)
+import kv from "@vercel/kv";
+import { Knewave } from "next/font/google";
 
-  return <main>{data.stargazers_count}</main>
+export default async function Home() {
+  const res = await fetch("https://api.github.com/repos/GreedECrow/class10", {
+    next: { revalidate: 15 },
+  });
+  const data = await res.json();
+  console.log(data);
+
+  const pageViews = await kv.incr("views");
+
+  return;
+  <main>
+    <p>0</p>
+    <span>⭐{data.stargazers_count}</span>
+  </main>;
 }
